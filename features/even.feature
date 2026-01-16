bb1da83c11ddcc94c7ecc476412806b68a8bad40
KecsMilan <kecskemeti.milan@diak.szbi-pg.hu>
2026-01-16 11:37:35 +0100

    push push
    box box

--- features/even.feature
+++ features/even.feature
@@ -2,6 +2,12 @@ Feature: Páros szám ellenőrzése
 
   # TODO: Írd ide a scenariókat!
   # 1. Páros szám ellenőrzése (pl. 4)
+
+  Scenario: Páros szám ellenőrzés
+    Given the number is 4
+    When I check the number
+    Then I should told "even"
+
   # 2. Páratlan szám ellenőrzése (pl. 5)
   # 3. Nulla ellenőrzése (0)
   # 4. Negatív páros szám ellenőrzése (-4)
--- features/steps/even_steps.py
+++ features/steps/even_steps.py
@@ -1,21 +1,22 @@
 from behave import given, when, then
+from src.number_checker import check_number
 # TODO: Importáld a number_checker modult a src mappából
 
 
 # TODO: Implementáld a Given step-et
-# @given('the number is {number}')
-# def step_given_number(context, number):
-#     pass
-
+@given('the number is 4')
+def step_given_number(context):
+    context.num = 4
 
 # TODO: Implementáld a When step-et
 # Használd a check_number függvényt a src/number_checker.py fájlból!
-# @when('I check the number')
-# def step_when_check_number(context):
-#     pass
+@when('I check the number')
+def step_when_check_number(context):
+    context.answer = check_number(context.num)
 
 
 # TODO: Implementáld a Then step-et
-# @then('the result should be "{expected}"')
-# def step_then_result(context, expected):
-#     pass
+@then('I should told "{expected}"')
+def step_then_result(context, expected):
+    assert context.answer == expected, \
+        f"Expected '{expected}', but got '{context.answer}'"
